c18b9166baf0dfc3ea6690f66fdf66e31598b387
Roberto <robertomdev@gmail.com>
2024-08-21 17:06:01 +0200

    * Refactor on run_besu_with_bootnodes.sh to make it more readable.

--- multiple-nodes/node-discovery/run_besu_with_bootnodes.sh
+++ multiple-nodes/node-discovery/run_besu_with_bootnodes.sh
@@ -1,116 +1,170 @@
 #!/bin/bash
 
-# Handles an initial delay based on a delay variable (Default value is 3)
+# input variables
+declare -r ALL_ARGS=("$@")
+
+# const variables
 declare INITIAL_DELAY=3
-for arg in "$@"; do
-    # Check if the argument starts with "delay="
+declare -r MAX_RETRIES=3
+declare -r RETRY_SLEEP=3
+declare -r SKIP_MISSING_BOOTNODES=true
+
+echo ""
+
+# Initial delay
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+# Handles an initial delay based on a delay variable (Default value is 3)
+#   | Check if the argument starts with "delay="
+#   | Extract the value after the equals sign
+for arg in "${ALL_ARGS[@]}"; do
     if [[ $arg == delay=* ]]; then
-        # Extract the value after the equals sign
         INITIAL_DELAY="${arg#delay=}"
     fi
 done
-echo "Initial delay of $INITIAL_DELAY"
+echo "Initial delay of $INITIAL_DELAY seconds"
 sleep $INITIAL_DELAY
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
-declare -r RETRY_SLEEP=3
-declare -r MAX_RETRIES=3
-declare -r SKIP_MISSING_BOOTNODES=true
+# Filter Besu args
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+# Filter all args to keep only Besu specific ones
+#   | Check if the argument starts with "--"
+for arg in "${ALL_ARGS[@]}"; do
+  if [[ $arg == --* ]]; then
+    BESU_ARGUMENTS+=("$arg")
+  fi
+done
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-# Filter to keep only Besu arguments
-function filter_besu_args() {
-  for arg in "$1"; do
-      if [[ $arg == --* ]]; then
-          filtered_args+=("$arg")
-      fi
-  done
-  echo $filtered_args
+# Process bootnodes arg
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+# Process the bootnodes argument into an array
+for arg in "${ALL_ARGS[@]}"; do
+  if [[ $arg == bootnodes=* ]]; then
+    BOOTNODES_ARGS=${arg#bootnodes=}
+    IFS=',' read -r -a BOOTNODES_ARGS_ARRAY <<< "$BOOTNODES_ARGS"
+  fi
+done
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+
+# Defining some functions
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+function obtain_enode_id() {
+  url=$1
+  echo $(curl -s -X POST --data '{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":1}' "$url" | grep -o '"enode":"[^"]*' | sed 's/"enode":"enode:\/\/\([^@]*\)@.*/\1/')
+}
+
+function build_enode() {
+  ENODE_ID=$1
+  IP_ADDRESS=$2
+  echo "enode://$ENODE_ID@$IP_ADDRESS:30303"
+}
+
+function run_besu() {
+  declare -r BESU_ARGUMENTS=$1
+  declare -r ENODES=$2
+
+  echo "Starting Besu node..."
+  echo "With args:: $BESU_ARGUMENTS"
+  echo "With bootnodes:: $ENODES"
+
+  /opt/besu/bin/besu --bootnodes="$ENODES" "$BESU_ARGUMENTS"
 }
 
 function print_warning_skipped_bootnode() {
-  URL=$1
+  url=$1
   echo ""
   echo "*** *** *** *** *** *** *** ***"
   echo "          WARNING!"
   echo "--- --- --- --- --- --- --- ---"
-  echo "Couldn't get enode url for $URL"
+  echo "Couldn't get enode for $url"
   echo "This bootnode will be skipped"
   echo "*** *** *** *** *** *** *** ***"
   echo ""
 }
-
-echo ""
-
-# Process the bootnodes argument into an array
-for arg in "$@"; do
-  if [[ $arg == bootnodes=* ]]; then
-    bootnodes=${arg#bootnodes=}
-    IFS=',' read -r -a bootnode_array <<< "$bootnodes"
-  fi
-done
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 
-# Prints the bootnodes
+# Finish if no bootnodes
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 echo "Solving bootnodes:"
-if [ ${#bootnode_array[@]} -eq 0 ]; then
+if [ ${#BOOTNODES_ARGS_ARRAY[@]} -eq 0 ]; then
   echo "No bootnodes were defined."
-else
-  successful_enodes=()
-  for bootnode in "${bootnode_array[@]}"; do
 
-    echo "Attempting to get enode for $bootnode"
+  if [ $SKIP_MISSING_BOOTNODES != "true" ]; then
+    echo "Defining bootnodes is required."
+    echo ""
+    exit 1
+  fi
 
-    declare num_retries=0
-    declare found_enode=false
-    declare ENODE_ID=""
-    declare URL=""
-    while [ $num_retries -le $MAX_RETRIES ] && [ $found_enode = "false" ]; do
-      URL="http://$bootnode"
-      ENODE_ID=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":1}' "$URL" | grep -o '"enode":"[^"]*' | sed 's/"enode":"enode:\/\/\([^@]*\)@.*/\1/')
+  run_besu "$BESU_ARGUMENTS"
+  echo ""
+  exit 0
+fi
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-      echo "ENODE_ID: $ENODE_ID"
 
-      if [ -z "$ENODE_ID" ] || [ -z "${ENODE_ID+x}" ] || [ "$ENODE_ID" = "null" ]; then
-        if [ $num_retries -eq $MAX_RETRIES ]; then
-          if [ $SKIP_MISSING_BOOTNODES = "true" ]; then
-            print_warning_skipped_bootnode $URL
-          else
-            echo "Couldn't get enode url for $URL"
-            exit 1
-          fi
+# Process bootnodes
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+declare successful_enodes=()
+
+for bootnode in "${BOOTNODES_ARGS_ARRAY[@]}"; do
+  echo "Attempting to get enode for $bootnode"
+
+  declare num_retries=0
+  declare found_enode=false
+
+  while [ $num_retries -le $MAX_RETRIES ] && [ $found_enode = "false" ]; do
+    url="http://$bootnode"
+    enode_id=$(obtain_enode_id $url)
+
+    echo "enode_id: $enode_id"
+
+    if [ -z "$enode_id" ] || [ -z "${enode_id+x}" ] || [ "$enode_id" = "null" ]; then   # If no enode_id...
+      if [ $num_retries -eq $MAX_RETRIES ]; then
+        if [ $SKIP_MISSING_BOOTNODES = "true" ]; then
+          print_warning_skipped_bootnode $url
         else
-          sleep $RETRY_SLEEP
-          echo "retrying..."
+          echo "Couldn't get enode for $url"
+          echo ""
+          exit 1
         fi
       else
-        found_enode=true
-        ip_address=$(echo "$bootnode" | cut -d ':' -f 1)
-        successful_enodes+=("enode://$ENODE_ID@$ip_address:30303")
-
+        sleep $RETRY_SLEEP
+        echo "retrying..."
       fi
+    else  # If enode_id...
+      ip_address=$(echo "$bootnode" | cut -d ':' -f 1)   # Gets the host part of the bootnode (bootnode expected value pattern is <host>:<port>)
 
-      ((num_retries++))
-    done
+      found_enode=true  # Skip the while
+      successful_enodes+=($(build_enode $enode_id $ip_address))   # Adds the enode to a enodes list
+    fi
 
+    ((num_retries++))
   done
+done
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-  if [ ${#successful_enodes[@]} -eq 0 ] && [ $SKIP_MISSING_BOOTNODES != "true" ]; then
-    echo "No enodes were found..."
+
+# Process enodes
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+if [ ${#successful_enodes[@]} -eq 0 ]; then
+  echo "No enodes were found."
+  if [ $SKIP_MISSING_BOOTNODES != "true" ]; then
+    echo ""
     exit 1
-  else
-    # Filter all args to keep only Besu specific ones
-    BESU_ARGUMENTS=$(filter_besu_args $@)
-
-    # Converts successful_enodes array into string with comma separated values
-    IFS=',' enodes="${successful_enodes[*]}"
-
-    echo "Starting Besu node..."
-    echo "With args:: $BESU_ARGUMENTS"
-    echo "With enodes:: $enodes"
-
-    # Run Besu node
-    /opt/besu/bin/besu --bootnodes="$enodes" "$BESU_ARGUMENTS"
-    exit 0
   fi
+  echo "Skipping all bootnodes..."
+else
+  # Converts successful_enodes array into string with comma separated values
+  IFS=',' enodes="${successful_enodes[*]}"
 
+  # Run Besu node
+  run_besu "$BESU_ARGUMENTS" "$enodes"
 fi
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+
+echo ""
+exit 0
